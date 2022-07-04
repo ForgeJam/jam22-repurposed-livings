eb13537927d42ccce3cd8ed0b25866b419f6c7c0
Su5eD <su5ed@outlook.com>
2022-07-04 14:42:06 +0200

    Convert buildscript to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,96 @@
+import java.time.LocalDateTime
+
+plugins {
+    id("net.minecraftforge.gradle") version "5.1.+"
+    id("org.parchmentmc.librarian.forgegradle") version "1.+"
+}
+
+group = "wtf.gofancy.mc"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+minecraft {
+    mappings("parchment", "1.18.2-2022.07.03-1.19")
+    
+    runs {
+        create("client") {
+            workingDirectory = file("run").canonicalPath
+
+            properties(mapOf(
+                "forge.logging.markers" to "REGISTRIES",
+                "forge.logging.console.level" to "debug",
+                "forge.enabledGameTestNamespaces" to "repurposed-livings"
+            ))
+        }
+        
+        create("server") {
+            workingDirectory = file("run").canonicalPath
+            
+            properties(mapOf(
+                "forge.logging.markers" to "REGISTRIES",
+                "forge.logging.console.level" to "debug",
+                "forge.enabledGameTestNamespaces" to "repurposed-livings"
+            ))
+        }
+        
+        create("gameTestServer") {
+            workingDirectory = file("run").canonicalPath
+                    
+            properties(mapOf(
+                "forge.logging.markers" to "REGISTRIES",
+                "forge.logging.console.level" to "debug",
+                "forge.enabledGameTestNamespaces" to "repurposed-livings"
+            ))
+        }
+
+        create("data") {
+            workingDirectory = file("run").canonicalPath
+            
+            properties(mapOf(
+                "forge.logging.markers" to "REGISTRIES",
+                "forge.logging.console.level" to "debug"
+            ))
+            
+            args("--mod", "repurposedlivings", "--all", "--output", file("src/generated/resources/"), "--existing", file("src/main/resources/"))
+        }
+    }
+}
+
+sourceSets.main.configure { resources.srcDir("src/generated/resources") }
+
+repositories {
+    // Put repositories for dependencies here
+    // ForgeGradle automatically adds the Forge maven and Maven Central for you
+
+    // If you have mod jar dependencies in ./libs, you can declare them as a repository like so:
+    // flatDir {
+    //     dir 'libs'
+    // }
+}
+
+dependencies {
+    minecraft(group = "net.minecraftforge", name = "forge", version = "1.19-41.0.63")
+}
+
+// Example for how to get properties into the manifest for reading at runtime.
+tasks {
+    jar {
+        finalizedBy("reobfJar")
+        manifest.attributes(
+            "Specification-Title" to project.name,
+            "Specification-Vendor" to "Su5ed, Nikx, Silk",
+            "Specification-Version" to "1",
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to archiveVersion.get(),
+            "Implementation-Vendor" to "Su5ed, Nikx, Silk",
+            "Implementation-Timestamp" to LocalDateTime.now()
+        )
+    }
+    
+    withType(JavaCompile::class.java) {
+        options.encoding = "UTF-8"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven { 
+            name = "MinecraftForge"
+            url = uri("https://maven.minecraftforge.net")
+        }
+        maven { 
+            name = "Parchment"
+            url = uri("https://maven.parchmentmc.org")
+        }
+    }
+}
+
+rootProject.name = "repurposed-livings"
